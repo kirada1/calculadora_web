e9d7c1f2767cf48d51795372a78f19395e8d7b07
kirada1 <matheusmtg16@gmail.com>
2025-10-01 19:59:17 -0300

    comitando para dar marge

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/calcular", methods="POST")
+def caculo():
+    try:
+        numero1 = float(request.get("formulario", 0)) # type: ignore
+        numero2 = float(request.get("formulario", 0)) # type: ignore
+
+    except Exception as e:
+        return render_template("index.html", resultado=f"erro {e}")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
