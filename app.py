64dadc4131b20dedfc77f4903626a22c735ad122
kirada1 <matheusmtg16@gmail.com>
2025-10-02 18:23:53 -0300

    atualizando

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask , request, render_template, jsonify # type: ignore
+
 
 app = Flask(__name__)
 
@@ -6,16 +7,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
-
-
-@app.route("/calcular", methods="POST")
-def caculo():
-    try:
-        numero1 = float(request.get("formulario", 0)) # type: ignore
-        numero2 = float(request.get("formulario", 0)) # type: ignore
-
-    except Exception as e:
-        return render_template("index.html", resultado=f"erro {e}")
 
 
 if __name__ == "__main__":
