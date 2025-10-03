e96c1f9d6488b35181506a9d41dab3db4adeaca9
kirada1 <matheusmtg16@gmail.com>
2025-10-03 16:56:54 -0300

    logica da calculadora

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask , request, render_template, jsonify # type: ignore
+from flask import Flask, render_template, request, session  # type: ignore
 
 
 app = Flask(__name__)
+app.secret_key = "primeira key"
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+
+    if "tecla" not in session:
+        session["tecla"] = ""
+
+    if request.method == "POST":
+        tecla = request.form.get("key")
+
+        if tecla == "c":
+            session["tecla"] = ""
+
+        elif tecla == "backspace":
+
+            try:
+                session["tecla"] = session["tecla"][:-1]
+            except Exception as e:
+                return f"nada para apagar {e}"
+
+        elif tecla == "=":
+            try:
+                session["tecla"] = str(eval(session["tecla"]))
+            except Exception:
+                session["tecla"] = "erro"
+
+        else:
+            session["tecla"] += tecla
+
+    return render_template("index.html", expressao=session["tecla"])
 
 
 if __name__ == "__main__":
